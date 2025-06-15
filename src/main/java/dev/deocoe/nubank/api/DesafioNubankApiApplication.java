6180f24ebadb19a96254b66c43d79344477fcfdb
Daniel Coelho <deocoe@gmail.com>
2025-06-15 16:18:31 -0300

    chore: cria estrutura inicial do projeto com Spring Boot

---
+++ src/main/java/dev/deocoe/nubank/api/DesafioNubankApiApplication.java
@@ -0,0 +1,13 @@
+package dev.deocoe.nubank.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioNubankApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioNubankApiApplication.class, args);
+    }
+
+}
